10c2b12ac1cea072bcc49248e84acca499958269
Fathima Mehrin V S <mehrinfiza57@gmail.com>
2026-06-26 12:51:24 +0530

    Implemented JWT for role based access

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from fastapi import FastAPI,Query,HTTPException
+from fastapi import FastAPI,Query,HTTPException,Depends
 from typing import Annotated
 from pydantic import BaseModel,Field
 from datetime import datetime
 from enum import Enum
-class Login(BaseModel):
-    username:str|None=None
-    email_id:str
-    password:str
+from jose import jwt
+from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
+SECRET_KEY="projecthub_backend_fastapi_2026"
+ALGORITHM="HS256"
+oauth2_scheme=OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login/")
 class Projects(BaseModel):
     title:str=Field(min_length=1)
     description:str|None=None
@@ -58,12 +59,47 @@ users=[
     }
 ]
 projects=[]
+#create token function:
+def create_access_token(data):
+    return jwt.encode(
+        data,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+#decoding token (verifying token)
+def verify_token(token):
+    payload=jwt.decode(
+        token,
+        SECRET_KEY,
+        algorithms=[ALGORITHM]
+    )
+    return payload
+def checkuser(data):
+    for i in users:
+        if((i["email"]==data.username) and (i["password"]==data.password)):
+                return i
+    return None
 #find user : to  be later used in create-project
 def find_user(id):
     for i in users:
         if i["id"]==id:
             return(i)
-        
+def get_current_user(token=Depends(oauth2_scheme)):
+    try:
+        payload=verify_token(token)
+    except:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid or expired token"
+        )
+    user_id=payload["user_id"]
+    user_record=find_user(user_id)
+    if(user_record is None):
+        raise HTTPException(
+            status_code=401,
+            detail="User not found!"
+        )
+    return user_record       
 #register function
 @app.post("/api/v1/auth/register/")
 async def register(userReg:UserRegister):
@@ -79,65 +115,48 @@ async def register(userReg:UserRegister):
     return new_user
 #login feature
 @app.post("/api/v1/auth/login/")
-async def login(data:Login,name:Annotated[str|None,Query(max_length=50)]=None):
-    user=checkuser(data)
+async def login(form_data:OAuth2PasswordRequestForm=Depends()):
+    user=checkuser(form_data)
     if user is None:
         raise HTTPException(
             status_code=401,
             detail="Unauthorized error ! Email and Password doesnt match"
         )
-    else:
-        data_dict=data.model_dump()
-        if data.email_id=="admin@company.com":
-            data_dict.update({"Admin":"Full access: create, read, update, delete any project"})
-        elif data.email_id=="manager@company.com":
-            data_dict.update({"Manager":"Create,Read,update any project--Cannot delete"})
-        else:
-            data_dict.update({"User":"Create projects; read/update only own projects"})
-        if name:
-            data_dict.update({"Name":name})
-        return data_dict
-    
+    payload = {
+        "user_id" : user["id"],
+        "email" : user["email"],
+        "role":user["role"]
+    }
+    token=create_access_token(payload)
+    return {
+        "access_token":token,
+        "token_type":"bearer"
+    }
 #reading the users
 @app.get("/api/v1/users/")
 async def read_users():
     #for accessing a global parameter, dont use query parameter
     return users
-#finding role
-def find_role(id):
-    for i in users:
-        if i["id"]==id:
-            return i["role"]
 #create projects
 @app.post("/api/v1/create_projects")
-async def create_project(user_id:int,project:Projects):
-    owner=find_user(user_id)
-    if owner is None:
-        raise HTTPException(
-            status_code=404,
-            detail="User with this ID doesnot exist"
-        )
+async def create_project(project:Projects,user=Depends(get_current_user)):
     new_project={
         "id":len(projects)+1,
         "title":project.title,
         "description":project.description,
         "status":"draft",
-        "owner_id":owner["id"],
-        "owner_username":owner["username"],
+        "owner_id":user["id"],
+        "owner_username":user["username"],
         "created_at":datetime.now(),
         "updated_at":datetime.now()
     }
     projects.append(new_project)
     return new_project
 #read projects
-@app.get("/api/v1/read_projects/{id}")
-async def read_projects(id:int):
-    now_role=find_role(id)
-    if(now_role==None):
-        raise HTTPException(
-            status_code=404,
-            detail="User not found"
-        )
+@app.get("/api/v1/read_projects")
+async def read_projects(user=Depends(get_current_user)):
+    id=user["id"]
+    now_role=user["role"]
     if(now_role=="admin" or now_role=="manager"):
         return projects
     else:
@@ -147,22 +166,17 @@ def find_project(pid):
     for i in projects:
         if(i["id"]==pid):
             return i
-@app.get("/api/v1/projects/{project_id}/{uid}")
-async def get_project(project_id:int,uid:int):
+@app.get("/api/v1/projects/{project_id}")
+async def get_project(project_id:int,user=Depends(get_current_user)):
     #here we are passing the project id
+    uid=user["id"]
     project=find_project(project_id)
     if project is None:
         raise HTTPException(
             status_code=404,
             detail="Project not found"
         )
-    user=find_user(uid)
-    if user is None:
-        raise HTTPException(
-            status_code=404,
-            detail="User not found"
-        )
-    role=find_role(uid)
+    role=user["role"]
     if(project["owner_id"]==uid or role=="admin" or role=="manager"):
         return project
     else:
@@ -171,8 +185,6 @@ async def get_project(project_id:int,uid:int):
             detail="403 Forbidden- Unauthorized access"
         )
 #JWT Authentication
-def checkuser(data):
-    for i in users:
-        if((i["email"]==data.email_id) and (i["password"]==data.password)):
-                return i
-    return None
+@app.get("/api/v1/auth/me")
+async def current_user(user_record=Depends(get_current_user)):
+    return user_record 
